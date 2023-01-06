eb74542763bad8afd714ad96e79562bd291f72bf
muradhossin <murad15-2547@diu.edu.bd>
2023-01-06 15:35:36 +0600

    internet connectivity check

--- lib/webview_page.dart
+++ lib/webview_page.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -9,34 +12,83 @@ class WebviewPage extends StatefulWidget {
 }
 
 class _WebviewPageState extends State<WebviewPage> {
-
+  late StreamSubscription<ConnectivityResult> subscription;
   late WebViewController _webViewController;
+  bool _isConnected = true;
+
+  @override
+  void initState() {
+    isConnectedToInternet().then((value) {
+      setState(() {
+        _isConnected = value;
+      });
+    });
+    subscription = Connectivity().onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.wifi ||
+          result == ConnectivityResult.mobile) {
+        setState(() {
+          _isConnected = true;
+        });
+      } else {
+        setState(() {
+          _isConnected = false;
+        });
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(0.5),
-        child: AppBar(
-
-        ),
+        child: AppBar(),
       ),
       body: WillPopScope(
-        onWillPop: () async{
-          if(await _webViewController.canGoBack()){
+        onWillPop: () async {
+          if (await _webViewController.canGoBack()) {
             _webViewController.goBack();
             return false;
-          }else{
+          } else {
             return true;
           }
         },
-        child: WebView(
-          initialUrl: "https://sites.google.com/diu.edu.bd/muradhossin/home",
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (controller) {
-            _webViewController = controller;
-          },
-        ),
+        child: _isConnected
+            ? WebView(
+                initialUrl:
+                    "https://sites.google.com/diu.edu.bd/muradhossin/home",
+                javascriptMode: JavascriptMode.unrestricted,
+                onWebViewCreated: (controller) {
+                  _webViewController = controller;
+                },
+              )
+            : Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Icon(
+                      Icons.signal_wifi_connected_no_internet_4_outlined,
+                      size: 50,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Please check your internet connection...',
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
+  }
+
+  Future<bool> isConnectedToInternet() async {
+    var result = await Connectivity().checkConnectivity();
+    return result == ConnectivityResult.wifi || result == ConnectivityResult.mobile;
   }
 }
